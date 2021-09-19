fa2264e6d9e447315ab5ef608e754fd3817971a5
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-09-19 23:37:27 +0200

    Convert codebase to POSIX, remove bash dependency

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 REPOSITORY_OWNER="$INPUT_REPOSITORY_OWNER"
 REPOSITORY_NAME="$INPUT_REPOSITORY_NAME"
@@ -7,7 +7,7 @@ PULL_REQUEST="$INPUT_PULL_REQUEST"
 __LINKED_ISSUES_HTML=""
 
 
-function prepareInputs {
+prepareInputs() {
   if [ -z "$REPOSITORY_OWNER" ]; then
     REPOSITORY_OWNER=$(cat "$GITHUB_EVENT_PATH" | jq -r ".repository.owner.login")
   fi;
@@ -27,29 +27,32 @@ function prepareInputs {
   fi;
 }
 
-
-function extractLinkedIssuesHTML {
+extractLinkedIssuesHTML() {
   URL="https://github.com/$REPOSITORY_OWNER/$REPOSITORY_NAME/pull/$PULL_REQUEST/"
+  inside_form_substring="<form class=\"js-issue-sidebar-form"
+  outside_form_substring="</form>"
 
+  echo "$(curl -sSL --retry 3 "$URL")" > \
+     /tmp/pr-linked-issues-action-response.txt
   _INSIDE_FORM=0
-  while read -r line ; do
+  while IFS= read -r line; do
     if [ $_INSIDE_FORM -eq 0 ]; then
-      if [[ $line == *"<form class=\"js-issue-sidebar-form"* ]]; then
+      if test "${line#*$inside_form_substring}" != "$line"; then
         _INSIDE_FORM=1
-      fi;
+      fi
     else
-      if [[ $line == *"</form>"* ]]; then
+      if test "${line#*$outside_form_substring}" != "$line"; then
         _INSIDE_FORM=0
       else
         _LINKED_ISSUES_HTML="$_LINKED_ISSUES_HTML
 $line"
       fi;
     fi;
-  done < <(curl -sSL --retry 3 "$URL")
+  done < /tmp/pr-linked-issues-action-response.txt
 }
 
 
-function main() {
+main() {
   prepareInputs
 
   extractLinkedIssuesHTML
@@ -63,7 +66,8 @@ function main() {
       | tr "\n" ","
   )
   if [ ! -z  "$ISSUES" ]; then
-    ISSUES="${ISSUES::-1}"
+    #ISSUES="${ISSUES::-1}"
+    ISSUES=${ISSUES%,}
   fi;
   echo "::set-output name=issues::$ISSUES"
 }
