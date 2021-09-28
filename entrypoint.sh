9466e36485ead2055fd6057b75c8f7c422098583
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-09-28 15:07:20 +0200

    Use GraphQL Github API to retrieve linked issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ REPOSITORY_OWNER="$INPUT_REPOSITORY_OWNER"
 REPOSITORY_NAME="$INPUT_REPOSITORY_NAME"
 PULL_REQUEST="$INPUT_PULL_REQUEST"
 
-__LINKED_ISSUES_HTML=""
-
 
 prepareInputs() {
   if [ -z "$REPOSITORY_OWNER" ]; then
@@ -27,49 +25,17 @@ prepareInputs() {
   fi;
 }
 
-extractLinkedIssuesHTML() {
-  URL="https://github.com/$REPOSITORY_OWNER/$REPOSITORY_NAME/pull/$PULL_REQUEST/"
-  inside_form_substring="<form class=\"js-issue-sidebar-form"
-  outside_form_substring="</form>"
-
-  echo "$(curl -sSL --retry 3 "$URL")" > \
-     /tmp/pr-linked-issues-action-response.txt
-  _INSIDE_FORM=0
-  while IFS= read -r line; do
-    if [ $_INSIDE_FORM -eq 0 ]; then
-      if test "${line#*$inside_form_substring}" != "$line"; then
-        _INSIDE_FORM=1
-      fi
-    else
-      if test "${line#*$outside_form_substring}" != "$line"; then
-        _INSIDE_FORM=0
-      else
-        _LINKED_ISSUES_HTML="$_LINKED_ISSUES_HTML
-$line"
-      fi;
-    fi;
-  done < /tmp/pr-linked-issues-action-response.txt
-}
-
 
 main() {
   prepareInputs
 
-  extractLinkedIssuesHTML
-
-  # extract issues numbers
-  ISSUES=$(
-    echo "$_LINKED_ISSUES_HTML" \
-      | grep -o "data-hovercard-url=\"/$REPOSITORY_OWNER/$REPOSITORY_NAME/issues/[0-9]\+/" \
-      | cut -d'/' -f5 \
-      | sort -u \
-      | tr "\n" ","
-  )
-  if [ ! -z  "$ISSUES" ]; then
-    #ISSUES="${ISSUES::-1}"
-    ISSUES=${ISSUES%,}
-  fi;
-  echo "::set-output name=issues::$ISSUES"
+  script="query{repository(name:\\\"$REPOSITORY_NAME\\\",owner:\\\"$REPOSITORY_OWNER\\\"){pullRequest(number:$PULL_REQUEST){closingIssuesReferences(first:100){nodes{number}}}}}"
+  response="$(curl -s -H 'Content-Type: application/json' \
+    -H "Authorization: bearer $GITHUB_TOKEN" \
+    -X POST -d "{ \"query\": \"$script\"}" https://api.github.com/graphql)"
+  echo ::set-output name=issues::$(echo $response \
+    | jq .data.repository.pullRequest.closingIssuesReferences.nodes[].number? \
+    | tr "\n" "," | sed 's/,$//')
 }
 
 
